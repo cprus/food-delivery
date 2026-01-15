b9fa7c790cbb2ecf54ab1cbf0d1deb7ccf0966e4
Collin P <collinprusse@mac.com>
2026-01-15 15:20:34 -0700

    initial setup and homescreen

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Auth Layout() {
+  return (
+    <View>
+      <Text>Auth Layout</Text>
+    </View>
+  )
+}
